2ddf6d53b19161575d3521d5a33c8ef6552a7cb2
Dotan Alter <dotan@stream.security>
2025-10-23 17:16:48 +0300

    change azure module required provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     streamsec = {
       source  = "streamsec-terraform/streamsec"
-      version = ">= 1.8"
+      version = ">= 1.13.2"
     }
     azuread = {
       source  = "hashicorp/azuread"
